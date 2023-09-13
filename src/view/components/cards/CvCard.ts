7fd042be4e645ddee1675fb5afd4ee3422057f2d
dev641 <dev641science@gmail.com>
2023-09-13 10:46:02 +0530

    download corrected

--- src/view/components/cards/CvCard.ts
+++ src/view/components/cards/CvCard.ts
@@ -5,7 +5,7 @@ const cvCard: render = ({ heading, download, contact }: CvCard) => {
   return `
     <div class="cv-card card dark-card" id="cv-card">
       <p class="heading cv-card__heading" id="cv-card__heading">${heading}</p>
-        <a href="${download.src}" download=${download.extension} class="cv-card__download btn" id="cv-card__download">${download.name}</a>
+        <a href="${download.src}" download="${download.extension}" class="cv-card__download btn" id="cv-card__download">${download.name}</a>
         <a href="${contact.src}" class="cv-card__contact-me btn" id="cv-card__contact-me">${contact.name}</a>
     </div>
   `
