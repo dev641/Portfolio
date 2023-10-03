caffc6f252959631268c1477148fca4ea3bf21e5
dev641 <dev641science@gmail.com>
2023-10-03 21:43:07 +0530

    Analytics Added

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,9 @@ import Portfolio from './controller/PortfolioController'
 import PortfolioModel from './model/PortfolioModel'
 import './styles/main.scss'
 import { PortfolioView } from './view/PortfolioView'
+import { inject } from '@vercel/analytics'
 
+inject()
 // eslint-disable-next-line no-new
 new Portfolio(PortfolioModel, new PortfolioView(PortfolioModel.Data, PortfolioModel.Theme))
 // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
