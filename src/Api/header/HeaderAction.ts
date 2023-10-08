db3c54deff6aba07fabebfdb0df3bf4fef7ea009
dev641 <dev641science@gmail.com>
2023-10-08 13:28:57 +0530

    Header Json file name change

--- src/Api/header/HeaderAction.ts
+++ src/Api/header/HeaderAction.ts
@@ -1,4 +1,4 @@
-import { brand, menu, theme as th } from '../../data/menu/menu.json'
+import { brand, menu, theme as th } from '../../data/Header/Header.json'
 import { ThemeEnum } from '../../model/Enum'
 const theme = th === 'dark' ? ThemeEnum.dark : ThemeEnum.light
 export default { brand, menu, theme }
