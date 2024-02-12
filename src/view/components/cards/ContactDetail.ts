a073908027fa8918fb4b0917438002bbb42c904a
dev641 <dev641science@gmail.com>
2024-02-13 01:05:29 +0530

    reduntant block removed

--- src/view/components/cards/ContactDetail.ts
+++ src/view/components/cards/ContactDetail.ts
@@ -1,28 +1,6 @@
 import { ContactDetails, render } from "../../../types/cards";
 import { formatPhoneNumber } from "../../../util/util";
 
-{{
-    // Here is a reference snippet of code from src\view\components\cards\CvCard.ts:
-//
-// import { type CvCard } from './../../../types/cards.d'
-// import { type render } from '../../../types/cards'
-//
-// const cvCard: render = ({ heading, download, contact }: CvCard) => {
-//   return `
-//     <div class="cv-card card dark-card" id="cv-card">
-//       <p class="heading cv-card__heading" id="cv-card__heading">${heading}</p>
-//         <a href="${download.src}" download="${download.extension}" class="cv-card__download btn" id="cv-card__download">${download.name}</a>
-//         <a href="${contact.src}" class="cv-card__contact-me btn" id="cv-card__contact-me">${contact.name}</a>
-//     </div>
-//   `
-// }
-//
-// export default cvCard
-//
-//
-// Here is a reference snippet of code from src\types\cards.d.
-}}
-
 const contactDetailsCard: render = ({
     image,
     name,
